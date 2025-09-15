db9e755e50c87d521dddde870537c442aa901757
Amara Irobi <beaamara6@gmail.com>
2025-09-15 17:54:44 -0400

    Initial commit: created Flutter project

---
+++ lib/main.dart
@@ -0,0 +1,160 @@
+import 'package:flutter/material.dart'; // import: need to access a lib that's defined in dart, standard library used for 90% of apps, gives access to visual components: app bar, scaffold, and text
+
+void main() { 
+  runApp(MyApp());  // main entry point in flutter app, tells flutter to start building/displaying MyApp widget
+}
+
+class MyApp extends StatelessWidget { // the constructor, the main initiation for objects to be used w/ variables and methods
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(  // grab all the common features for theme, nav and the title
+      home: DefaultTabController( // default screen for app, makes coordination for the tabs
+        length: 4,
+        child: _TabsNonScrollableDemo(), // deal with tab bar/view inside widget
+      ),
+    );
+  }
+}
+
+class _TabsNonScrollableDemo extends StatefulWidget { // makes content like tapping work
+  @override
+  __TabsNonScrollableDemoState createState() => __TabsNonScrollableDemoState();
+}
+
+class __TabsNonScrollableDemoState extends State<_TabsNonScrollableDemo>
+    with SingleTickerProviderStateMixin, RestorationMixin {
+  late TabController _tabController; // brain, keeps track of all the tabs.
+  
+  final RestorableInt tabIndex = RestorableInt(0);
+
+  @override
+  String get restorationId => 'tab_non_scrollable_demo';
+  
+  @override
+  void restoreState(RestorationBucket? oldBucket, bool initialRestore) { // restore change/ keep stuff the same
+    registerForRestoration(tabIndex, 'tab_index');
+    _tabController.index = tabIndex.value;
+  }
+
+  @override
+  void initState() { // life cycle methods, flutter makes the calls automatically
+    super.initState();
+    _tabController = TabController(
+      initialIndex: 0,
+      length: 4,
+      vsync: this, // required to make animation run smoothly
+    );
+    _tabController.addListener(() {
+      setState(() {
+        tabIndex.value = _tabController.index;
+      });
+    });
+  }
+
+  @override
+  void dispose() { // when you change or create, get rid of the old one and load/keep the new one
+    _tabController.dispose(); // release all the resources, prevents memory leaks
+    tabIndex.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // For the To do task hint: consider defining the widget and name of the tabs here
+    final tabs = ['Tab 1', 'Tab 2', 'Tab 3', 'Tab 4'];
+
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Text('Class Activity 01: Tabs'),
+        bottom: TabBar(
+          controller: _tabController,
+          isScrollable: false,
+          tabs: [for (final tab in tabs) Tab(text: tab)],
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController, 
+        children: [
+   
+          Container( // container for colors
+            color: Colors.orange[100],
+            child: Center(
+              child: GestureDetector(
+                onTap: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: Text("Alert"),
+                      content: Text("This is this tab's alert dialog."),
+                      actions: [
+                        TextButton(
+                          onPressed: () => Navigator.pop(context),
+                          child: Text("OK"),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+                child: Text(
+                  "Welcome to Tab 1",
+                  style: TextStyle(fontSize: 20, fontStyle: FontStyle.italic),
+                ),
+              ),
+            ),
+          ),
+
+   
+          Container(
+            color: Colors.green[100],
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextField(
+                    decoration: InputDecoration(labelText: "Enter text here"),
+                  ),
+                  SizedBox(height: 20),
+                  Image.network(
+                    'https://images.unsplash.com/photo-1464660439080-b79116909ce7?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8c3Vuc2hpbmV8ZW58MHx8MHx8fDA%3D',
+                    width: 250,
+                    height: 250,
+                  ),
+                ],
+              ),
+            ),
+          ),
+
+         
+          Container(
+            color: Colors.blue[100],
+            child: Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text("Button pressed in Tab 3!")),
+                  );
+                },
+                child: Text("Click me"),
+              ),
+            ),
+          ),
+
+       
+          Container( 
+            color: Colors.yellow[100],
+            child: ListView(
+              padding: EdgeInsets.all(8),
+              children: [
+                Card(child: ListTile(title: Text("Item 1"))),
+                Card(child: ListTile(title: Text("Item 2"))),
+                Card(child: ListTile(title: Text("Item 3"))),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
